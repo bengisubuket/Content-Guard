7d886859f857443015153568ed6ac4354dbc80f3
bengisubuket <bengisubuket@icloud.com>
2023-12-18 15:24:34 +0300

    Input keyword is fixed

--- content-guard/src/components/keyword-blocker/KeywordBlocker.jsx
+++ content-guard/src/components/keyword-blocker/KeywordBlocker.jsx
@@ -1,63 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Row, Col, Form, Dropdown, Image } from 'react-bootstrap';
-import { PlusCircleFill } from 'react-bootstrap-icons';
-import { ArrowLeft } from 'react-bootstrap-icons';
+import { PlusCircleFill, ArrowLeft } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router-dom';
 
 function KeywordBlockerComponent() {
   const navigate = useNavigate();
+  const [keyword, setKeyword] = useState('');
+  const [keywordsList, setKeywordsList] = useState([]);
 
   const navigateBack = () => {
-      navigate('/blockers');
+    navigate('/blockers');
+  };
+
+  const handleAddKeyword = () => {
+    if (keyword.trim() !== '') {
+      setKeywordsList([...keywordsList, keyword]);
+      setKeyword('');
+    }
   };
 
   return (
     <Container>
       <Row className='mb-3'>
-          <Col>
-              <Image src="guardian.png" roundedCircle style={{ width: '100px', height: '100px', margin: '0 auto' }} />
-          </Col>
-          <Col style={{ display: 'flex', alignItems: 'center' }}>
-              <div style={{ fontSize: '24px' }}>Keyword Blocker</div>
-          </Col>
+        <Col>
+          <Image src="guardian.png" roundedCircle style={{ width: '100px', height: '100px', margin: '0 auto' }} />
+        </Col>
+        <Col style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ fontSize: '24px' }}>Keyword Blocker</div>
+        </Col>
       </Row>
       <Row className='mb-3'>
-          <Col xs={9}>
-              <Form.Control type="text" placeholder="Enter keyword"/>
-          </Col>
-          <Col className="text-end">
-              <Button>
-                  <PlusCircleFill style={{ fontSize: '24px', color: 'blue' }} />
-              </Button>
-          </Col>
+        <Col xs={9}>
+          <Form.Control
+            type="text"
+            placeholder="Enter keyword"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+        </Col>
+        <Col className="text-end">
+          <Button onClick={handleAddKeyword}>
+            <PlusCircleFill style={{ fontSize: '24px', color: 'blue' }} />
+          </Button>
+        </Col>
       </Row>
       <Row className='mb-3'>
-          <Dropdown>
-              <Dropdown.Toggle id="dropdown-basic" style={{ width: '100%' }}>
-                  Keywords
-              </Dropdown.Toggle>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-basic" style={{ width: '100%' }}>
+            Keywords
+          </Dropdown.Toggle>
 
-              <Dropdown.Menu style={{ width: '100%' }}>
-                  <Dropdown.Item>keyword0</Dropdown.Item>
-                  <Dropdown.Item>keyword1</Dropdown.Item>
-                  <Dropdown.Item>keyword2</Dropdown.Item>
-              </Dropdown.Menu>
-          </Dropdown>
+          <Dropdown.Menu style={{ width: '100%' }}>
+            {keywordsList.map((kw, index) => (
+              <Dropdown.Item key={index}>{kw}</Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
       </Row>
       <Row>
         <Button
-            onClick={navigateBack}
-            variant="primary"
-            style={{
+          onClick={navigateBack}
+          variant="primary"
+          style={{
             backgroundColor: 'transparent',
             border: 'none',
             padding: 2,
             width: 'auto',
-            }}
+          }}
         >
-            <ArrowLeft style={{ fontSize: '24px', color: 'blue' }} />
+          <ArrowLeft style={{ fontSize: '24px', color: 'blue' }} />
         </Button>
-    </Row>
+      </Row>
     </Container>
   );
 }
