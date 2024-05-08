bd0f8119a2a137fcb4f26e5ad4c211e46b12a0aa
İlayda Zehra Yılmaz <zehra.yilmaz@ug.bilkent.edu.tr>
2024-05-08 22:28:17 +0300

    deletes

--- content-guard-home/src/layouts/dashboard/index.js
+++ content-guard-home/src/layouts/dashboard/index.js
@@ -78,8 +78,8 @@ function Dashboard() {
       <VuiBox py={3}>
 
         <VuiBox mb={3}>
-          <Grid container spacing="18px">
-            <Grid item xs={12} lg={12} xl={5}>
+        <Grid container spacing={3} justifyContent="center">
+            <Grid item xs={12} lg={6} xl={7}>
               <WelcomeMark />
             </Grid>
 
@@ -87,7 +87,7 @@ function Dashboard() {
         </VuiBox>
        
         <VuiBox mb={3}>
-          <Grid container spacing={3}>
+        <Grid container spacing={3} justifyContent="center">
 
             <Grid item xs={12} lg={6} xl={7}>
               <Card>
